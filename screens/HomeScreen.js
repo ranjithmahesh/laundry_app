3f4d3a4767419618292ec0a07bfe2925b1c1964a
Ranjith <ranjithsm459@gmail.com.com>
2023-09-21 20:09:30 +0530

    Created a new Expo app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
